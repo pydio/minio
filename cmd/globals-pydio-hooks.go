94ce586ca9d57d82c345089b11cb03c44c53b575
charles <charles@pydio.com>
2021-11-22 16:45:07 +0100

    Additional hooks

--- cmd/globals-pydio-hooks.go
+++ cmd/globals-pydio-hooks.go
@@ -20,9 +20,77 @@
 
 package cmd
 
-import "github.com/gorilla/mux"
+import (
+	"context"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/minio/minio/internal/auth"
+	"github.com/minio/minio/internal/disk"
+	"net/http"
+	"net/url"
+)
+
+type ReqParamExtractor func(req *http.Request, m map[string]string)
+
+var (
+	pydioReqParamHooks []ReqParamExtractor
+)
 
 // HookRegisterGlobalHandler provides an access point to enrich globalHandlers
 func HookRegisterGlobalHandler(handlerFunc mux.MiddlewareFunc) {
 	globalHandlers = append(globalHandlers, handlerFunc)
+}
+
+// HookExtractReqParams registers a request parameters extractor, used in the event system
+func HookExtractReqParams(extractor ReqParamExtractor) {
+	pydioReqParamHooks = append(pydioReqParamHooks, extractor)
+}
+
+func applyHooksExtractReqParams(req *http.Request, m map[string]string) {
+	for _, f := range pydioReqParamHooks {
+		f(req, m)
+	}
+}
+
+// ExposedParseSignV4 parses a v4 signature and return the signature accessKey if it's valid.
+func ExposedParseSignV4(v4auth string) (string, error) {
+	val, code := parseSignV4(v4auth, globalServerRegion, "s3")
+	if code != ErrNone {
+		return "", fmt.Errorf("cannot parse signature - code is %d", code)
+	} else {
+		return val.Credential.accessKey, nil
+	}
+}
+
+// ExposedWriteErrorResponse writes an error code in proper XML foramt
+func ExposedWriteErrorResponse(ctx context.Context, w http.ResponseWriter, code APIErrorCode, reqURL *url.URL) {
+	writeErrorResponse(ctx, w, errorCodes.ToAPIErr(code), reqURL)
+}
+
+// ExposedStoreTmpUserToIAMSys stores an idToken as valid accessKey in the globalIAMSys
+func ExposedStoreTmpUserToIAMSys(idToken string) error {
+	return globalIAMSys.SetTempUser(idToken, auth.Credentials{}, "")
+}
+
+// ExposedDiskStats returns info about the disk
+func ExposedDiskStats(ctx context.Context, fsPath string, health bool) (map[string]interface{}, error) {
+	stats := map[string]interface{}{}
+	i, e := disk.GetInfo(fsPath)
+	if e != nil {
+		return stats, e
+	}
+	stats["Total"] = i.Total
+	stats["Free"] = i.Free
+	stats["Used"] = i.Used
+	stats["FSType"] = i.FSType
+
+	if health {
+		lat, tp, e := disk.GetHealthInfo(ctx, "", fsPath)
+		if e != nil {
+			return stats, e
+		}
+		stats["Health.Latency"] = lat
+		stats["Health.Throughput"] = tp
+	}
+	return stats, nil
 }
--- cmd/handler-utils.go
+++ cmd/handler-utils.go
@@ -240,6 +240,8 @@ func extractReqParams(r *http.Request) map[string]string {
 	if _, ok := r.Header[xhttp.MinIOSourceReplicationRequest]; ok {
 		m[xhttp.MinIOSourceReplicationRequest] = ""
 	}
+	// Specific Hook Pydio
+	applyHooksExtractReqParams(r, m)
 	return m
 }
 
